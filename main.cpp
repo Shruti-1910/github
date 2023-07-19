1116906daebd3c5465a79adb07baef15cb259c61
Shruti-1910 <skshrutikumari.46@gmail.com>
2023-07-19 18:59:46 +0530

    even or  odd number

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdtr1c++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a number.";
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<"Even number";
+    }
+    else
+    cout<<"Odd number";
+    return 0;
+}
